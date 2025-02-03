cb3ffdc6a9fa149871e82c44178ceb353be9f103
m154-m M <230013353@aston.ac.uk>
2025-02-03 19:49:51 +0000

    completed task1 on lab in task1.hs

--- lab2-functionalprimer/haskell/task1.hs
+++ lab2-functionalprimer/haskell/task1.hs
@@ -3,9 +3,10 @@ module Main where
 main =
     do
     print (myFunc1 5)
-    --print (MyFunc2 10)
-    --print (3rdFunc)
+    print (myFunc2 10)
+    print (_3rdFunc)
 
+-- function names must start with lowercase or underscore and only contain letters, digits, underscore, or apostrophe.
 myFunc1 x = x*10
---MyFunc2 x = x*2
---3rdFunc = "Hello, this is 3rd Func"
+myFunc2 x = x*2   
+_3rdFunc = "Hello, this is 3rd Func"
