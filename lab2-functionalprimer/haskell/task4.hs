f667cdb10d8ac22bf487b19c07929407b93f069b
m154-m M <230013353@aston.ac.uk>
2025-02-03 21:20:39 +0000

    completed task4 on lab in task4.hs

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -4,9 +4,14 @@ ask prompt =
   putStrLn prompt
   line <- getLine
   if line == ""
-    then ask prompt
+    then ask ( prompt ++ "!")
+    else if line == "quit"
+      then putStrLn "quitting..."    
     else putStrLn ("you said: " ++ reverse line)
 
+-- ii) checks if the input is "quit" and exits the loop after displaying "quitting..." (lines 8&9)
+-- iii) promt for recurssion: if empty, then promt, again (++) conctanate ! (line 7)
+ 
 main :: IO ()
 main =
   do
